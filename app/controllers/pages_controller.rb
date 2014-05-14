22f7cb9c92e7bf1648222b639bcd50415705c4e5
Blake Taylor <blakefrost@gmail.com>
2014-05-13 22:25:19 -0400

    Quick hack to show editions instead of publications

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   def home
-    @publications = Publication.all
+    #@publications = Publication.all
+    @editions = Edition.all
     render layout: false
   end
 end
