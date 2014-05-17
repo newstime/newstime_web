091bd788a3b0a20ef4e3de1f4a0ea6a2c8f32241
Blake Taylor <blakefrost@gmail.com>
2014-05-16 23:47:57 -0400

    Clean up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,8 @@
 Newsstand::Application.routes.draw do
 
   devise_for :users
+
   root to: "pages#home"
-
-  #devise_for :users
-
-  namespace :newsstand do
-  resources :editions
-  end
-
-  scope '/editions', controller: 'editions' do
-    get '/' => :index, as: :editions
-    get '/new' => :new, as: :new_edition
-
-    #get '/:id' => :show, as: :edition
-    get "/*path" => "editions#show", as: :edition
-
-    post '/' => :create
-  end
-
-  scope '/store', controller: 'store_items' do
-    get '/' => :index, as: :store_items
-    get "/*path" => :show
-  end
 
   scope ":publication_slug" do
     get ':edition_slug' => 'publications#show'
