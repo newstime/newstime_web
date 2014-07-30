3412a761654492aec357fd1e2d97853cae7cfbf2
Blake Taylor <blakefrost@gmail.com>
2014-07-29 21:43:04 -0400

    Divide up paths by subdomain

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Newsstand::Application.config.session_store :cookie_store, key: '_newsstand_session'
+Newsstand::Application.config.session_store :cookie_store, key: '_newsstand_session', domain: 'newstime.io'
--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,64 @@
 Newsstand::Application.routes.draw do
 
-  devise_for :users, :controllers => { :sessions => "sessions" }
+  constraints subdomain: "www" do
 
-  root to: "pages#home"
+    devise_for :users, :controllers => { :sessions => "sessions" }
 
-  get 'search'    => 'search#search' #, constraints: { :q => /.+/ }
-  get 'newsrack'  => "pages#newsrack"
-  get 'downloads' => 'downloads#download'
-  get 'library' => 'libraries#show'
+    root to: "pages#home"
 
-  get 'newsrack_sprite/:path' => 'newsracks#sprite'
+    get 'search'    => 'search#search' #, constraints: { :q => /.+/ }
+    get 'newsrack'  => "pages#newsrack"
+    get 'downloads' => 'downloads#download'
+    get 'library' => 'libraries#show'
 
-  resources :news_organization_inquiries, only: [:new, :create] do
-    collection do
-      get :success
+    get 'newsrack_sprite/:path' => 'newsracks#sprite'
+
+    resources :news_organization_inquiries, only: [:new, :create] do
+      collection do
+        get :success
+      end
     end
-  end
 
-  get "/organization" => "organizations#show"
+    get "/organization" => "organizations#show"
 
-  resources :editions do
-    member do
-      put :purchase
-      get :checkout
+    resources :editions do
+      member do
+        put :purchase
+        get :checkout
+      end
     end
-  end
 
-  resources :publications do
-    member do
-      put :subscribe
+    resources :publications do
+      member do
+        put :subscribe
+      end
     end
+
+    scope ":publication_slug" do
+      get ':edition_slug' => 'editions#show'
+      get ':edition_slug/*path' => 'editions#browse'
+
+      post ':edition_slug' => 'editions#create'
+    end
+
+    get 'account'         => 'accounts#show'
+    get 'wallet'          => 'accounts#wallet'   ,       as: :account_wallet
+    get 'purchases'       => 'accounts#purchases',       as: :account_purchases
+    get 'subscriptions'   => 'accounts#subscriptions',   as: :account_subscriptions
+    get 'change_password' => 'accounts#change_password', as: :account_change_password
+    get 'update_info'     => 'accounts#update_info', as: :account_update_info
+
+
+    get "*path", :to => "pages#show"
+
   end
 
-  scope ":publication_slug" do
-    get ':edition_slug' => 'editions#show'
-    get ':edition_slug/*path' => 'editions#browse'
+  constraints subdomain: "read" do
 
-    post ':edition_slug' => 'editions#create'
+    scope ":publication_slug" do
+      get ':edition_slug/*path' => 'editions#browse'
+    end
+
   end
-
-  get 'account'         => 'accounts#show'
-  get 'wallet'          => 'accounts#wallet'   ,       as: :account_wallet
-  get 'purchases'       => 'accounts#purchases',       as: :account_purchases
-  get 'subscriptions'   => 'accounts#subscriptions',   as: :account_subscriptions
-  get 'change_password' => 'accounts#change_password', as: :account_change_password
-  get 'update_info'     => 'accounts#update_info', as: :account_update_info
-
-
-  get "*path", :to => "pages#show"
 
 end
