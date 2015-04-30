2fc486904033237043cb09e224c50aad4e3dadde
Blake Taylor <blakefrost@gmail.com>
2015-04-30 19:43:05 -0400

    Guard against nil edition and publication for desc result

--- app/models/edition_copy_purchase_wallet_transaction.rb
+++ app/models/edition_copy_purchase_wallet_transaction.rb
@@ -5,11 +5,13 @@ class EditionCopyPurchaseWalletTransaction < WalletTransaction
   has_one :publication, through: :edition
 
   def desc_html
-    <<-HTML
-      Purchase
-      #{ActionController::Base.helpers.link_to edition.name, edition.url}
-      of
-      #{ActionController::Base.helpers.link_to publication.name, publication.url}
-    HTML
+    if edition && publication
+      <<-HTML
+        Purchase
+        #{ActionController::Base.helpers.link_to edition.name, edition.url}
+        of
+        #{ActionController::Base.helpers.link_to publication.name, publication.url}
+      HTML
+    end
   end
 end
